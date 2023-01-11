ea709271a52e78c2d71b9942f968029606db1fd2
ethcala <81577564+ethcala@users.noreply.github.com>
2023-01-11 14:11:40 -0700

    framework for backend methods in dal

---
+++ Controllers/ApiController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace cfr_backend.Controllers
+{
+    public class ApiController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Controllers/UserController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace cfr_backend.Controllers
+{
+    public class UserController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
